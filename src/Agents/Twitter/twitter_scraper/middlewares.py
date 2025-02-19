e3dc80d4e1cdd1e01f88a3898acbc98926beda9b
felixpoirier1 <felixpoirier2001@gmail.com>
2025-02-19 12:52:03 -0500

    Update gitignore to ignore Kalshi sensitive information

--- src/Agents/Twitter/twitter_scraper/middlewares.py
+++
@@ -1,8 +0,0 @@
-# twitter_scraper/twitter_scraper/middlewares.py
-import logging
-
-class RedirectLoggerMiddleware:
-    def process_spider_output(self, response, result, spider):
-        logging.info(f"Final URL: {response.url}")
-        for item in result:
-            yield item
